57cf040d59ccb7a69ff3c3f35df70fff166ab057
ibuprofeno@chromebook <ibuprofenofernandez@gmail.com>
2023-10-04 12:06:44 +0200

    upgrade

--- entregas/fernandezIbuprofeno/reto-002/CalcularCambio.java
+++ entregas/fernandezIbuprofeno/reto-002/CalcularCambio.java
@@ -1,5 +1,5 @@
-class CambioMejorado{
-    public static void main(String[] args){
+class CambioMejorado {
+    public static void main(String[] args) {
 
         int entrega = 400;
         int debe = 232;
@@ -9,39 +9,39 @@ class CambioMejorado{
         String mensaje = "";
 
         ahoraTrabajando = 100;
-        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
-        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
-        mensaje = mensaje + ((cantidadBilletes>0)?cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n":"");
+        cantidadBilletes = pendientePorDevolver / ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver % ahoraTrabajando;
+        mensaje = mensaje + ((cantidadBilletes > 0) ? cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n" : "");
 
         ahoraTrabajando = 50;
-        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
-        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
-        mensaje = mensaje + ((cantidadBilletes>0)?cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n":"");
+        cantidadBilletes = pendientePorDevolver / ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver % ahoraTrabajando;
+        mensaje = mensaje + ((cantidadBilletes > 0) ? cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n" : "");
 
         ahoraTrabajando = 20;
-        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
-        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
-        mensaje = mensaje + ((cantidadBilletes>0)?cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n":"");
+        cantidadBilletes = pendientePorDevolver / ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver % ahoraTrabajando;
+        mensaje = mensaje + ((cantidadBilletes > 0) ? cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n" : "");
 
         ahoraTrabajando = 10;
-        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
-        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
-        mensaje = mensaje + ((cantidadBilletes>0)?cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n":"");
+        cantidadBilletes = pendientePorDevolver / ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver % ahoraTrabajando;
+        mensaje = mensaje + ((cantidadBilletes > 0) ? cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n" : "");
 
         ahoraTrabajando = 5;
-        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
-        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
-        mensaje = mensaje + ((cantidadBilletes>0)?cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n":"");
+        cantidadBilletes = pendientePorDevolver / ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver % ahoraTrabajando;
+        mensaje = mensaje + ((cantidadBilletes > 0) ? cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n" : "");
 
         ahoraTrabajando = 2;
-        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
-        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
-        mensaje = mensaje + ((cantidadBilletes>0)?cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n":"");
+        cantidadBilletes = pendientePorDevolver / ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver % ahoraTrabajando;
+        mensaje = mensaje + ((cantidadBilletes > 0) ? cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n" : "");
 
         ahoraTrabajando = 1;
-        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
-        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
-        mensaje = mensaje + ((cantidadBilletes>0)?cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n":"");
+        cantidadBilletes = pendientePorDevolver / ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver % ahoraTrabajando;
+        mensaje = mensaje + ((cantidadBilletes > 0) ? cantidadBilletes + " billetes de " + ahoraTrabajando + "€\n" : "");
 
         System.out.println(mensaje);
     }
