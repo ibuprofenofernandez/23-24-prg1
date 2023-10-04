1dce2a7843de9a4323b70ce8fff2e7f5ca7b8f29
Ibuprofeno Fernández <99534750+ibuprofenofernandez@users.noreply.github.com>
2023-10-04 11:47:09 +0200

    Update CalcularCambio.java

--- entregas/fernandezIbuprofeno/reto-001/CalcularCambio.java
+++ entregas/fernandezIbuprofeno/reto-001/CalcularCambio.java
@@ -1,3 +1,45 @@
-class CalcularCambio{
-  
+class Cambio{
+    public static void main(String[] args){
+
+        int entrega = 400;
+        int debe = 232;
+        int ahoraTrabajando;
+        int cantidadBilletes;
+        int pendientePorDevolver = entrega - debe;
+
+        ahoraTrabajando = 100;
+        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
+        System.out.println(cantidadBilletes + " billetes de " + ahoraTrabajando + "€");
+
+        ahoraTrabajando = 50;
+        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
+        System.out.println(cantidadBilletes + " billetes de " + ahoraTrabajando + "€");
+
+        ahoraTrabajando = 20;
+        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
+        System.out.println(cantidadBilletes + " billetes de " + ahoraTrabajando + "€");
+
+        ahoraTrabajando = 10;
+        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
+        System.out.println(cantidadBilletes + " billetes de " + ahoraTrabajando + "€");
+
+        ahoraTrabajando = 5;
+        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
+        System.out.println(cantidadBilletes + " billetes de " + ahoraTrabajando + "€");
+
+        ahoraTrabajando = 2;
+        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
+        System.out.println(cantidadBilletes + " billetes de " + ahoraTrabajando + "€");
+
+        ahoraTrabajando = 1;
+        cantidadBilletes = pendientePorDevolver/ahoraTrabajando;
+        pendientePorDevolver = pendientePorDevolver%ahoraTrabajando;
+        System.out.println(cantidadBilletes + " billetes de " + ahoraTrabajando + "€");
+    }
 }
